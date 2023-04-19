000c7d79385fd5628bf4c761849bd1e5a60f78e4
wukaize <347570295@qq.com>
2023-04-19 17:47:38 +0800

    init

---
+++ src/views/index.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+
+const count = ref(0)
+</script>
+
+<template>
+    <div >
+			<div>
+				left
+			</div>
+			<div>
+				right
+			</div>
+		</div>
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
